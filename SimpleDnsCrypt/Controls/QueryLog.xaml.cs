f33e0d04ee9af8b8aa629815f969d0e1c9e72127
SamLS42 <sammyls@protonmail.com>
2025-11-08 23:00:53 +0100

    wip: restoring title bar

---
+++ SimpleDnsCrypt/Controls/QueryLog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SimpleDnsCrypt.Controls
+{
+	/// <summary>
+	/// Interaction logic for QueryLog.xaml
+	/// </summary>
+	public partial class QueryLog : UserControl
+	{
+		public QueryLog()
+		{
+			InitializeComponent();
+		}
+	}
+}
